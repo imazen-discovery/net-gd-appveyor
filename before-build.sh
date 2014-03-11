908047dc5f6e7eb7da9d819cce06f653ae69d9e1
Chris Reuter <chris@blit.ca>
2014-03-11 12:55:44 -0400

    Added before-build.sh step.

--- before-build.sh
+++ before-build.sh
@@ -2,5 +2,15 @@
 
 set -e
 
-echo 'before-build.sh run.'
+PWD=`pwd`
+
+# clone the repos
+git clone https://github.com/imazen-discovery/gd-libgd.git
+git clone https://github.com/imazen-discovery/net-gd.git
+
+# Install third party stuff from a tarball.  TODO: switch to using
+# chocolatey for this, since it's all there.
+pushd /c/
+zcat $PWD/files/tools.tar.gz | tar xvf -
+popd
 
